8ac7d5324a4ce6e0105fdedeb29ae067d994b3e4
rlittle <rlittle@littleacres.org>
2016-11-24 11:46:26 -0500

    Added AccountConverter to AccountManager Made AccountManager.list static Removed @Override annotation from PaymentView.getLedgerManager() method Changed object type in PaymentView.cbStore and PaymentView.cbAccount from SelectItem to Store and Account respectively Added StoreConvert to StoresManager Removed unused imports from LedgerView and DetailViewController

---
+++ src/com/webfront/uvtool/controller/AccountViewController.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webfront.uvtool.controller;
+
+import com.webfront.uvtool.model.Account;
+import com.webfront.uvtool.model.Server;
+import com.webfront.uvtool.util.Config;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author rlittle
+ */
+public class AccountViewController implements ControllerInterface, Initializable {
+
+    @FXML
+    Button btnSave;
+    @FXML
+    Button btnCancel;
+    @FXML
+    Button btnDelete;
+    @FXML
+    Label lblStatusMessage;
+    @FXML
+    TextField txtName;
+    @FXML
+    TextField txtPath;
+    @FXML
+    ComboBox cbServers;
+
+    ResourceBundle res;
+    private final Config config = Config.getInstance();
+
+    /**
+     * Initializes the controller class.
+     */
+
+    public AccountViewController() {
+        btnSave = new Button();
+        btnCancel = new Button();
+        lblStatusMessage = new Label();
+        txtName = new TextField();
+        txtPath = new TextField();
+        cbServers = new ComboBox();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        res = rb;
+        cbServers.getItems().addAll(config.getServers());
+    }
+    
+    @FXML
+    public void btnSaveOnAction() {
+        Server server = (Server) cbServers.getSelectionModel().getSelectedItem();
+        String name = txtName.getText();
+        String path = txtPath.getText();
+        if(server==null) {
+            lblStatusMessage.setText(res.getString("errServerSelect"));
+            return;
+        } else if(name.isEmpty()) {
+            lblStatusMessage.setText(res.getString("errNoAccountPath"));
+            return;
+        } else if(path.isEmpty()) {
+            lblStatusMessage.setText(res.getString("errNoPath"));
+            return;
+        }
+        for(Account a : config.getAccounts()) {
+            if(name.equals(a.getName()) && a.getServerName().equalsIgnoreCase(server.getName())) {
+                lblStatusMessage.setText(res.getString("errAccountExist"));
+                return;
+            }
+        }
+        Account account = new Account();
+        account.setServerName(server.getName());
+        account.setName(name);
+        account.setPath(path);
+        config.addAccount(account);
+    }
+
+    @Override
+    public Button getCancelButton() {
+        return btnCancel;
+    }
+
+    @Override
+    public void launch(String v, String t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ src/com/webfront/uvtool/controller/ServerViewController.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webfront.uvtool.controller;
+
+import com.webfront.uvtool.model.Server;
+import com.webfront.uvtool.util.Config;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author rlittle
+ */
+public class ServerViewController implements ControllerInterface, Initializable {
+
+    @FXML
+    Button btnSave;
+    @FXML
+    Button btnCancel;
+
+    @FXML
+    TextField txtHostName;
+    @FXML
+    TextField txtServerName;
+
+    @FXML
+    Label lblStatusMessage;
+
+    ResourceBundle res;
+
+    public ServerViewController() {
+        btnCancel = new Button();
+        btnSave = new Button();
+        lblStatusMessage = new Label();
+        txtHostName = new TextField();
+        txtServerName = new TextField();
+    }
+
+    /**
+     * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.res = rb;
+        txtHostName.setPromptText("Enter host name");
+        txtHostName.disableProperty().bind(txtServerName.textProperty().isEmpty());
+        this.btnSave.disableProperty().bind(this.txtHostName.textProperty().isEmpty());
+    }
+
+    @FXML
+    public void btnSaveOnAction() {
+        String serverName = txtServerName.getText();
+        String hostName = txtHostName.getText();
+        if (serverName.isEmpty()) {
+            lblStatusMessage.setText(res.getString("errNoServerName"));
+            txtServerName.requestFocus();
+        } else if (hostName.isEmpty()) {
+            lblStatusMessage.setText(res.getString("errNoHostName"));
+            txtHostName.requestFocus();
+        } else {
+            Config cfg = Config.getInstance();
+            lblStatusMessage.setText("");
+            Server s = new Server();
+            s.setHost(hostName);
+            s.setName(serverName);
+            cfg.addServer(s);
+            txtHostName.clear();
+            txtServerName.clear();
+            btnCancel.fire();
+        }
+    }
+
+    @Override
+    public Button getCancelButton() {
+        return btnCancel;
+    }
+
+    @Override
+    public void launch(String v, String t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
